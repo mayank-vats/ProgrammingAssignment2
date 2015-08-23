cff4df3c7ca47dbd423b5a243eb9a7cd83d3a84b
mayank_vats <gmlvats@gmail.com>
2015-08-23 13:06:45 +0530

    committing solution for programming Assignment 2 .

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Functions for fast repetitive calculation of a matrix 
+## inverse
+ 
 
-## Write a short comment describing this function
-
+##function used to return a list which  exposes methods to work with enclosed matrix and its inverse cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  #variable to cache inverse   
+  i <- NULL
+  
+  #function to set given matrix to x and reset cache to null
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  #get the matrix enclosed in this entity
+  get <- function() x
+  
+  #set the cache in this entity
+  setInverse <- function(inverse) i <<- inverse
+  
+  #get the cache in this entity for the enclosed matrix
+  getInverse <- function() i
+  
+  #list of methods exposed to work with enclosed matrix and its cached inverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##function used to calculate inverse for list output of makeCacheMatrix and cache it
+cacheSolve<- function(x, ...) {
+  
+  #check if inverse is already cached      
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  #inverse not cached,calculating
+  data <- x$get()
+  i <- solve(data, ...)
+  
+  #caching for further use
+  x$setInverse(i)
+  
+  #return
+  i
 }
